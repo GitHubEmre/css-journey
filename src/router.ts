84f60bf997cb09cd958fc3deceac12fe4a682596
Emre <emre.sener@outlook.fr>
2025-02-08 14:03:17 +0100

    fix module not found in router file

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import AnimationsComponent from './components/AnimationsComponent.vue';
-import FlexComponent from './components/FlexComponent.vue';
-import GridComponent from './components/GridComponent.vue';
-import HomeComponent from './components/HomeComponent.vue';
+// import AnimationsComponent from './components/AnimationsComponent.vue';
+// import FlexComponent from './components/FlexComponent.vue';
+// import GridComponent from './components/GridComponent.vue';
+// import HomeComponent from './components/HomeComponent.vue';
 import SelectorComponent from './components/SelectorComponent.vue';
 
 const routes: Array<RouteRecordRaw> = [
     { path: '/', redirect: '/selectors' },
-    { path: '/animations', name: 'Animations', component: AnimationsComponent },
-    { path: '/flex', name: 'Flexbox', component: FlexComponent },
-    { path: '/grid', name: 'Grid', component: GridComponent },
-    { path: '/home', name: 'Home', component: HomeComponent },
+    // { path: '/animations', name: 'Animations', component: AnimationsComponent },
+    // { path: '/flex', name: 'Flexbox', component: FlexComponent },
+    // { path: '/grid', name: 'Grid', component: GridComponent },
+    // { path: '/home', name: 'Home', component: HomeComponent },
     { path: '/selectors', name: 'Selectors', component: SelectorComponent },
 ];
 
