6d8d92406917ed30fcbe037f3061b7a534b518b9
Emre <emre.sener@outlook.fr>
2025-02-08 14:59:56 +0100

    fix development and production environment issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/css-journey/',
+  base: process.env.NODE_ENV === 'production' ? '/css-journey/' : '/',
   plugins: [vue()],
 })
